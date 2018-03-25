8a9b83c9426a66e99706e328b8c29d1cc1ec2d8d
h3dx0 <rob3rto@openmailbox.org>
2018-03-25 14:05:44 -0600

    login y registro listos

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ let index = require('./routes/index');
 let users = require('./routes/users');
 let nomencladores = require('./utiles/nomencladores');
 let cloudinary = require('cloudinary').v2;
+let passport = require('passport');
+let flash = require('connect-flash');
+let session = require('express-session');
+let mongoStore = require('connect-mongo')(session);
+
 let config = require('./config');
 //const fileUpload = require('express-fileupload');
+require('./config/passport');
 
 let app = express();
 
@@ -21,12 +27,32 @@ cloudinary.config({
     base_url: 'http://res.cloudinary.com/h3dx0'
 });
 
+app.use(session({
+    secret: 'mycodesecret',
+    resave: false,
+    saveUninitialized: false,
+    //guardando la session en la BD
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    }),
+    cookie: { maxAge: 180 * 60 * 1000}
+}));
+app.use(flash());
+
+
+//config d passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(function (req, res, next) {
     //para agregar la variable login y session a todas los view(.hbs)
     res.locals.gradosEscolares = nomencladores.gradosEscolares;
     res.locals.imagesUrl = config.imagesUrl;
+    res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
     next();
 });
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,7 +67,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,97 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let csrf = require('csurf');
+let csrfProtection = csrf();
+let passport = require('passport');
+let User = require('../models/user');
+router.use(csrfProtection);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/profile', isLoggedIn, function (req, res, next) {
+    res.render('user/profile', {user: req.user});
 });
 
+router.get('/profile/edit/personal-info', isLoggedIn, function (req, res, next) {
+    res.render('user/edit/edit_personal_info', {user: req.user, csrfToken: req.csrfToken()});
+});
+
+router.post('/profile/edit/personal-info', isLoggedIn, function (req, res, next) {
+    let name = req.body.name;
+    let firstName = req.body.firstName;
+    let email = req.body.email;
+    if (email === "" || name === "" || firstName === "") {
+        console.log('Campos vacios')
+        return res.render('user/edit/edit_personal_info', {user: req.user, csrfToken: req.csrfToken(), msg: 'No puede dejar ninugn campo vacio'});
+    }
+    let user = User.findById(req.user._id, function (err, user) {
+        if (err) {
+            console.log('Error al editar usuario')
+            res.render('user/edit/edit_personal_info', {user: req.user, csrfToken: req.csrfToken()});
+        }
+        user.name = name;
+        user.email = email;
+        user.firstName = firstName;
+        user.save(function (err, user) {
+            if (err) {
+                console.log('Error al editar usuario')
+                res.render('user/edit/edit_personal_info', {user: req.user, csrfToken: req.csrfToken()});
+            }
+            res.redirect('/user/profile');
+
+        })
+    });
+
+});
+
+router.get('/logout', isLoggedIn, function (req, res, next) {
+    req.logout();
+    res.redirect('/')
+});
+
+
+//esta ruta se aplica a todas las rutas q hay debajo de ellas  es decir aplica el middleware a todas
+router.use('/', notLoggedIn, function (req, res, next) {
+    return next();
+});
+
+router.get('/signup', function (req, res, next) {
+    let messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+//aqui aplicamos passport
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}));
+
+router.get('/signin', function (req, res, next) {
+    let messages = req.flash('error');
+    console.log(messages)
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+router.post('/signin', passport.authenticate('local.signin', {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}));
+
+
 module.exports = router;
+
+//proteger las rutas y el isAuthenticated es d passport
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/');
+    }
+}
